e6906fc36615003dbdccab323b2bf236c2d867b1
mimmouad <96013554+mimmouad@users.noreply.github.com>
2023-04-21 15:27:37 +0200

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,6 +15,12 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $request->user()->name=$request->get('name');
+        $request->user()->email=$request->get('email');
+       if($request->has('email')){
+           $request->user()->email=$request->get('email');
+        }
+
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
